930227b2e50c609d92f7d4934f178e51d4665717
ht_honey@163.com <ht_honey@163.com>
2021-04-27 10:48:49 +0800

    初始化

---
+++ config/routes.ts
@@ -0,0 +1,79 @@
+﻿export default [
+  {
+    path: '/',
+    component: '../layouts/BlankLayout',
+    routes: [
+      {
+        path: '/user',
+        component: '../layouts/UserLayout',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './User/login',
+          },
+        ],
+      },
+      {
+        path: '/',
+        component: '../layouts/SecurityLayout',
+        routes: [
+          {
+            path: '/',
+            component: '../layouts/BasicLayout',
+            authority: ['admin', 'user'],
+            routes: [
+              {
+                path: '/',
+                redirect: '/welcome',
+              },
+              {
+                path: '/welcome',
+                name: 'welcome',
+                icon: 'smile',
+                component: './Welcome',
+              },
+              {
+                path: '/admin',
+                name: 'admin',
+                icon: 'crown',
+                component: './Admin',
+                authority: ['admin'],
+                routes: [
+                  {
+                    path: '/admin/sub-page',
+                    name: 'sub-page',
+                    icon: 'smile',
+                    component: './Welcome',
+                    authority: ['admin'],
+                  },
+                ],
+              },
+              {
+                name: 'list.table-list',
+                icon: 'table',
+                path: '/list',
+                component: './TableList',
+              },
+              {
+                name: 'todo',//url中path部分，http://localhost:8000/todo状态跌幅到这个页面
+                path: '/todo',//名称，国际化菜单配置根据这个名称来配置，如果不配置将直菜单将直接显示这个名称
+                icon: 'unordered-list',//菜单图标名称
+                component:'./todo'// 组件（页面）相对于src/pages的路径
+              },
+              {
+                component: './404',
+              },
+            ],
+          },
+          {
+            component: './404',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    component: './404',
+  },
+];
